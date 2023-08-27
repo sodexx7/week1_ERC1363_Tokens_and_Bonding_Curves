63cf824fbf0aceaef890c99e8547ab93a82e349e
sodexx7 <sodexx7@gmail.com>
2023-08-27 22:11:37 +0800

    adjust again for workflow

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,14 +3,15 @@ pragma solidity ^0.8.17;
 
 import {Script} from "forge-std/Script.sol";
 
+import {TokenGodMod} from "src/TokenGodMod.sol";
 
 /// @notice A very simple deployment script
 contract Deploy is Script {
     /// @notice The main script entrypoint
     /// @return greeter The deployed contract
-    // function run() external returns (/** Greeter greeter*/ ) {
-    //     vm.startBroadcast();
-    //     // greeter = new Greeter("GM");
-    //     vm.stopBroadcast();
-    // }
+    function run() external returns (TokenGodMod tokenGodMod ) {
+        vm.startBroadcast();
+        tokenGodMod = new TokenGodMod();
+        vm.stopBroadcast();
+    }
 }
