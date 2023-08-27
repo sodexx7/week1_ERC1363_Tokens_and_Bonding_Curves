e982ca80a3134cb01940b05601c10ac70b42e21f
sodexx7 <sodexx7@gmail.com>
2023-08-27 22:15:04 +0800

    adjust again 2 for workflow

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity 0.8.9;
 
 import {Script} from "forge-std/Script.sol";
 
@@ -7,11 +7,14 @@ import {TokenGodMod} from "src/TokenGodMod.sol";
 
 /// @notice A very simple deployment script
 contract Deploy is Script {
+    address specialAddress = address(1234);
+
+
     /// @notice The main script entrypoint
-    /// @return greeter The deployed contract
+    /// @return tokenGodMod The deployed contract
     function run() external returns (TokenGodMod tokenGodMod ) {
         vm.startBroadcast();
-        tokenGodMod = new TokenGodMod();
+        tokenGodMod = new TokenGodMod(specialAddress,10_000);
         vm.stopBroadcast();
     }
 }
